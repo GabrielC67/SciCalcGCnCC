d143446de43772169a7231dc8f502b8e425b18fb
ConnorC04 <connorjcasey@iCloud.com>
2025-03-01 08:45:18 -0500

    Added new class Scientific and defined roles for project

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -2,6 +2,10 @@ package com.zipcodewilmington.scientificcalculator;
 
 import java.util.Scanner;
 
+// GABE WORKS IN THIS CLASS
+// CREATE SCIENTIFIC FUNCTIONS IN THIS CLASS
+// I DON'T MEAN TO YELL I JUST WANT TO CALL ATTENTION TO THIS
+
 public class Calculator {
 
 }
---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
@@ -0,0 +1,9 @@
+package com.zipcodewilmington.scientificcalculator;
+
+
+// CONNOR WORKS HERE
+// ADD FUNCTIONS FOR DISPLAY MODE, MEMORY, AND SWITCHING TRIG MODES HERE
+// STILL NOT YELLING JUST DON'T WANT THIS TO BE MISSED
+
+public class Scientific {
+}
