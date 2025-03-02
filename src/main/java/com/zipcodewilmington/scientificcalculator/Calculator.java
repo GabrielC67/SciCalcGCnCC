ec1428f680db738a668d28b62757c138d5ac6846
GabrielC67 <gabecruzpro@gmail.com>
2025-03-01 20:00:20 -0500

    Recent changes

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -1,7 +1,12 @@
-package com.zipcodewilmington.scientificcalculator;
+package com.zipcodewilmi
+public class Calculator{
+//This is #7 - Trigonometry Functions
+// Sine - Calculate the sine of the displayed value and display it
+    public static void main(String[] args){
+        //Ask myself, what do I need in order to calculate sine? Radius
 
-import java.util.Scanner;
 
-public class Calculator {
+        //System.out.println(Math.sin()); //This is what will be printed on the calculator
+    }
 
 }
